9e45ad5a70e64a8787f0981456b0f7e9d4909e90
espen <espen.erlandsen@gmail.com>
2026-01-09 15:20:19 +0100

    fix: resolve no-explicit-any warning in os.date shim

--- src/types_shim.d.ts
+++ src/types_shim.d.ts
@@ -9,6 +9,6 @@ declare interface ClassAccessorDecoratorContext { }
 declare namespace os {
     function time(): number;
     function clock(): number;
-    function date(fmt?: string, time?: number): any;
+    function date(fmt?: string, time?: number): string | { year: number; month: number; day: number; hour: number; min: number; sec: number; wday: number; yday: number; isdst: boolean; };
     function difftime(t2: number, t1: number): number;
 }
